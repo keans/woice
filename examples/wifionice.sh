0d816e5d1fe4a6803f8d84217595c88d720c7012
Ansgar Kellner <keans@gmx.de>
2018-01-08 18:23:02 +0100

    added wifionice.sh bash script to use with NetworkManager

---
+++ examples/wifionice.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+interface=$1
+status=$2
+
+# essid when script should be called
+ESSID="WIFIonICE"
+
+# interface for which script should be called
+INTERFACE="wlp1s0"
+
+# woice python script
+WOICE=/usr/local/bin/woice
+
+
+if [ "${interface}" = "${INTERFACE}" ]; then
+    # get current essid
+    essid=$(iwgetid -r)
+    
+    # get wifionice status
+    wifionicestatus=$(${WOICE} status)
+
+    if [ "${essid}" == "${ESSID}" ]; then
+        case "${status}" in
+            up)
+                if [ "${wifionicestatus}" == "down" ]; then
+                    logger Login to WIFI on ICE
+                    ${WOICE} up
+                fi
+                ;;
+            down)
+                if [ "${wifionicestatus}" == "up" ]; then
+                    logger Logout from WIFI on ICE
+                    ${WOICE} down
+                fi
+                ;;
+        esac
+    fi
+fi
+
